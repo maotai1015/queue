fd8536b7df36ebe17c5ddc7da4a30d58917d3420
zhangjiaxing <zhangjiaxing@xinye.com>
2022-10-25 15:53:52 +0800

    update

--- models/user.go
+++ models/user.go
@@ -1,1 +1,9 @@
 package models
+
+import "time"
+
+type User struct {
+	ID				int64		`json:"id" gorm:"primary_key"`
+	Phone			string		`json:"phone" gorm:"comment:手机号"`
+	CreateTime		time.Time	`json:"create_time" gorm:"comment:创建时间"`
+}
